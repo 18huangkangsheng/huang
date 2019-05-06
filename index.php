2151cd54caccec60c741a7b83cf2928cecb36cdb
18huangkangsheng <49503143+18huangkangsheng@users.noreply.github.com>
2019-05-06 23:06:39 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<HTML>
+<HEAD>
+<meta charset="utf-8"> 
+<TITLE></TITLE>
+</HEAD>
+<BODY>
+<fontsize=5>
+<?php
+	 function printbold($inputtext)
+	 {
+	 	print("<B>".$inputtext."</B>");
+	 }
+	 print("这行没有加重！<br>\n");
+	 printbold("这行加重了!!!");
+	 print("<br>\n");
+	 print("这行没有加重!<br>\n");
+?>
+</font>
+</BODY>
+</HTML>
